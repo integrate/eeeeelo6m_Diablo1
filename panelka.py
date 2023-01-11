e21ef40d990c03ab2757ff809037c53fd80cb8b3
Андрей <agd30072010@gmail.com>
2023-01-11 17:56:16 +0300

    Острова жадности

--- panelka.py
+++ panelka.py
@@ -1,12 +1,20 @@
 import pygame,settings,fullscreen
-
+from pygame import font
+font.init()
+print(font.get_fonts())
 class Panel():
-    def __init__(self,x=0,y=0):
+    def __init__(self,x=1,y=1,hp=110):
         self.x=x
         self.y=y
         self.w=settings.PANEL_SIZE_W
         self.h=pygame.display.get_surface().get_height()
         self.panel=pygame.Rect(self.x,self.y,self.w,self.h)
-    def draw(self,screen):
+        self.hp_bar=font.SysFont('segoeuiemoji',25,True)
+        self.hp_bar=self.hp_bar.render(str(hp),True,[0,0,0])
+        self.hp_bar_rect=pygame.Rect(self.panel.x+settings.PANEL_SIZE_W/6,25,settings.PANEL_SIZE_W/3*2,25)
+    def draw(self,screen:pygame.surface.Surface):
         a=fullscreen.fullscreen_rect(self.panel,screen,'war',False)
         pygame.draw.rect(screen,[134,145,221],a)
+        b=fullscreen.fullscreen_rect(self.hp_bar_rect,screen,'war',False)
+        pygame.draw.rect(screen,[255,255,255],b)
+        screen.blit(self.hp_bar,[b.centerx/self.hp_bar.get_width()+b.centerx,25])
