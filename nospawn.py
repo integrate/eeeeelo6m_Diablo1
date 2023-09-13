55f2d6263eb25f9d7a326d4ac43e58aadb1a158d
Андрей <agd30072010@gmail.com>
2023-09-13 17:37:04 +0300

    Острова жадности(дружба между мирами)

--- nospawn.py
+++ nospawn.py
@@ -1,6 +1,6 @@
 import pygame,fullscreen
 
-class Nospawn:
+class  Nospawn:
     def __init__(self,x,y,w,h,nospawn):
         self.rect_nospawn=pygame.Rect(x-nospawn, y-nospawn, w+nospawn*2, h+nospawn*2)
     def draw(self,screen,minimap):
